237825aaeb20538c97963b62d542fb47365d23bd
CN149 <cn149@CN149s-Mac-mini.local>
2019-06-10 14:14:02 +0530

    added productlist component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ selector: 'apm-root',
+ template: `
+  <div>
+    <h1>{{ pageTitle }}</h1>
+    <apm-products></apm-products>
+  </div>
+ ` 
 })
 export class AppComponent {
-  title = 'APM';
+  pageTitle: string = 'Acme Product Management';
 }
