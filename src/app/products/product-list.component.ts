388533416452857844e93fc16542f01ac60cb66e
Sagnik Chakraborti <saaggy18@gmail.com>
2019-06-17 00:43:11 +0530

    added product service

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    errorMessage: string;
 
     _listFilter: string;
     get listFilter(): string {
@@ -45,8 +46,13 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit() {
-      this.products = this.productService.getProducts();
-      this.filteredProducts = this.products;
+      this.productService.getProducts().subscribe(
+        products => {
+          this.products = products;
+          this.filteredProducts = this.products;
+        },
+        error => this.errorMessage = <any>error
+      )
     }
 
 }
